fbd59b5d4af7721844027d3fa063b605fa37c9d6
JAEHAN-source <64150526+JAEHAN-source@users.noreply.github.com>
2020-05-19 19:09:36 +0900

    middlewares : cookieParser,bodyParser,helmet,morgna

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+
 const app = express();
 
 const PORT = 4000;
@@ -7,15 +13,15 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-const middleware = (req, res, next) => {
-    console.log("between");
-    next();
-}
 
 const handleHome = (req, res) => res.send("welcome to home");
 const handleProfile = (req, res) => res.send("your profile");
 
-app.use(middleware);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);

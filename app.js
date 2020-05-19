940d84b04b816d719519afb5b34525efe3f31062
JAEHAN-source <64150526+JAEHAN-source@users.noreply.github.com>
2020-05-19 19:16:22 +0900

    basic format by init.js

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+
 
 
 const handleHome = (req, res) => res.send("welcome to home");
@@ -27,4 +25,6 @@ app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
 
-app.listen(PORT, handleListening);
+
+
+export default app;

404d839827ff6e09087189299a642c6efc519163
JAEHAN-source <64150526+JAEHAN-source@users.noreply.github.com>
2020-05-19 18:55:08 +0900

    middleware

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-const handleHome = (req, res) => res.send("welcome to home");
-
-function handleProfile(req, res) {
-    res.send("you are on my profile");
+const middleware = (req, res, next) => {
+    console.log("between");
+    next();
 }
+
+const handleHome = (req, res) => res.send("welcome to home");
+const handleProfile = (req, res) => res.send("your profile");
+
+app.use(middleware);
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);

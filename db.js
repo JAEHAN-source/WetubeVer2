9293dfbf4737863dd219f6d9b234bfc4e5726721
JAEHAN-source <64150526+JAEHAN-source@users.noreply.github.com>
2020-05-25 18:01:38 +0900

    using fake db and adding it using +mixin

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 2323232,
+        title: "video awasame",
+        description: "this is fake video",
+        views: 24,
+        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
+        creater: {
+            id: 22222,
+            name: "JAEHAN",
+            emai: "tlswogks0508@naver.com"
+        }
+    },
+    {
+        id: 23233232,
+        title: "video awasame",
+        description: "this is fake video",
+        views: 24,
+        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
+        creater: {
+            id: 22442,
+            name: "JAEHAN",
+            emai: "tlswogks0508@naver.com"
+        }
+    },
+    {
+        id: 23141432,
+        title: "video awasame",
+        description: "this is fake video",
+        views: 24,
+        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
+        creater: {
+            id: 22552,
+            name: "JAEHAN",
+            emai: "tlswogks0508@naver.com"
+        }
+    }
+]

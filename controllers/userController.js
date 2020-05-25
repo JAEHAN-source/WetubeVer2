8eeb4c1f17ce75782cf017278c60ecdd4129ba81
JAEHAN-source <64150526+JAEHAN-source@users.noreply.github.com>
2020-05-25 21:18:35 +0900

    devide join into get,post to get information

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,21 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+    const { body: { name, email, password1, password2 } } = req;
+    if (password1 !== password2) {
+        res.status(400);
+        res.render("join", { pageTitle: "Join" })
+
+    } else {
+        //to do:resister user
+        //to do:log in user
+        res.redirect(routes.home);
+    }
+
+};
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
